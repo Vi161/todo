b7d7d51baae4562c4b333cee76fad49dc269c04f
Vladimir Panov <vi161rus@gmail.com>
2019-09-10 22:41:11 +0300

    delete func

--- js/Edit.js
+++ js/Edit.js
@@ -1,10 +1,103 @@
+console.log('loaded edit.js');
+
+function TaskField() {
+    this.inputNew = document.querySelector('#inputNew');
+    this.buttonAdd = document.querySelector("#buttonAdd");
+    this.valueLength = function () {
+        return this.inputNew.addEventListener('input', function () {
+            this.value.length ? (buttonAdd.disabled = false) : (buttonAdd.disabled = true)
+        })
+    }
+}
+function Add() {
+    this.inputNew = document.querySelector('#inputNew');
+    this.button = document.querySelector('#buttonAdd');
+    this.list = document.querySelector('.form-wrap>ul');
+    this.addTaskToDOM = function () {
+        let id = 0;
+        let countId = function () {
+            return ++id;
+        };
+
+        let addElemToArr = function () {
+            store.data.arr.push({
+                id : id,
+                value : taskField.inputNew.value,
+                state : 1
+            });
+        };
+        let temp = function () {
+            add.list.insertAdjacentHTML('beforeend',
+                '<li class="task-item" id="'+store.data.arr[store.data.arr.length - 1].id+'"> ' +
+                '<button class="button delete">delete</button>' +
+                '<button class="button done">done</button> ' +
+                ' <p>'+ store.data.arr[store.data.arr.length - 1].value +'</p> ' +
+                '</li>');
+        };
+        // let updateElemDOM = function () {
+        //     console.log(store.data.arr.length);
+        //     let elem = document.querySelector('ul');
+        //     elem.innerHTML = "";
+        //     // elem.remove();
+        //     // document.createElement("ul");
+        //     for (let i = 0; i < store.data.arr.length; i++) {
+        //
+        //         // let newDiv = document.createElement("li");
+        //         // let newContent = document.createTextNode('');
+        //         // newDiv.appendChild(newContent)
+        //         // elem.appendChild(newDiv);
+        //
+        //         ul.insertAdjacentHTML('beforeend',
+        //             '<li class="task-item" id="'+store.data.arr[store.data.arr.length - 1].id+'"> ' +
+        //             '<button class="button delete">delete</button>' +
+        //             '<button class="button done">done</button> ' +
+        //             ' <p>'+ store.data.arr[i].value +'</p> ' +
+        //             '</li>');
+        //     }
+        //
+        // };
+        let onEnter = function () {
+            addElemToArr();
+            add.updateElemDOM();
+            taskField.inputNew.value = null;
+            buttonAdd.disabled = true;
+        };
+        return this.button.addEventListener('click', function () {
+                countId();
+                onEnter();
+            }) ||
+            this.inputNew.addEventListener("keyup", function(event) {
+                if (event.keyCode === 13) {
+                    if (taskField.inputNew.value !== '') {
+                        countId();
+                        onEnter();
+                    }
+                }
+            });
+    }
+    this.updateElemDOM = function () {
+        this.list.innerHTML = "";
+        for (let i = 0; i < store.data.arr.length; i++) {
+            add.list.insertAdjacentHTML('beforeend',
+                '<li class="task-item" id="'+store.data.arr[i].id+'"> ' +
+                '<button class="button delete">delete</button>' +
+                '<button class="button done">done</button> ' +
+                ' <p>'+ store.data.arr[i].value +'</p> ' +
+                '</li>');
+        }
+
+    };
+}
+
+
+
 function Edit() {
     this.elem = document.querySelector('ul');
     this.done = function() {
         this.elem.addEventListener('click', function (event) {
             if (event.target.className == 'button done') {
-                var doneElementText = document.querySelector('#'+event.target.parentNode.id +'>p');
-                var doneElementButton = document.querySelector('#'+event.target.parentNode.id +' .done');
+                let doneElementText = document.querySelector('#'+event.target.parentNode.id +'>p');
+                let doneElementButton = document.querySelector('#'+event.target.parentNode.id +' .done');
                 doneElementButton.disabled = true;
                 doneElementText.className += 'task-done';
                 console.log('edit', doneElementButton)
@@ -14,9 +107,14 @@ function Edit() {
     this.delete = function () {
         this.elem.addEventListener('click', function (event) {
             if (event.target.className == 'button delete') {
-                store.data.arr.splice(1,1);
+                let selfId = event.target.parentNode.id;
+                console.log('id=', store.data.arr.filter(item => item.id != selfId));
+                // store.data.arr.splice(1,1);
+                store.data.arr = store.data.arr.filter(item => item.id != selfId);
+
+                add.updateElemDOM();
                 console.log(store.data.arr)
-                // var deleteElement = document.querySelector('#'+event.target.parentNode.id);
+                // let deleteElement = document.querySelector('#'+event.target.parentNode.id);
                 // deleteElement.parentNode.removeChild(deleteElement);
                 // console.log('id for del ',event.target.parentNode.id);
                 // store.data.arr.splice(event.target.parentNode.id, 1);
@@ -25,7 +123,12 @@ function Edit() {
         });
     }
 }
+let add = new Add();
+let taskField = new TaskField();
+let store = new Store();
 
+add.addTaskToDOM();
+taskField.valueLength();
 edit = new Edit();
 edit.done();
 edit.delete();
